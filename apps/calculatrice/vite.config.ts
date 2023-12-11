5223bd43ee707c9ff7cae856ee8e65fa5c1404fe
Chloé <chloe.gommard@intest.info>
2023-12-11 13:55:50 +0100

    feat: restrcuture cypress

--- apps/calculatrice/vite.config.ts
+++ apps/calculatrice/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/calculatrice',
+  server: {
+    port: 5174,
+  },
   test: {
     environment: 'jsdom',
     globals: true,
